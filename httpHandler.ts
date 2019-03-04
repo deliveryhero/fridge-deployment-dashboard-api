6549d1d4dd81eee19bc3a8165718b90a611a95b2
Mohamed Azouz <mohamedaliazouz@gmail.com>
2019-03-04 15:01:43 +0100

    T-2 add deployment model with tests

--- httpHandler.ts
+++ httpHandler.ts
@@ -1,41 +1,41 @@
-import { Context, Handler } from 'aws-lambda';
-import { NestFactory } from '@nestjs/core';
-import { Server } from 'http';
+import {Context, Handler} from 'aws-lambda';
+import {NestFactory} from '@nestjs/core';
+import {Server} from 'http';
 import * as serverless from 'aws-serverless-express';
 import {AppModule} from './src/app.module';
-import { ValidationPipe} from '@nestjs/common';
-const express = require('express')();
+import {ValidationPipe} from '@nestjs/common';
 import * as bodyParser from 'body-parser';
-// import {config} from './src/application-configuration-factory';
+
+const express = require('express')();
 let cachedServer: Server;
 
 export const handle: Handler = (event: AWSLambda.APIGatewayEvent, context: Context) => {
-    if (!cachedServer) {
-        bootstrapServer().then(server => {
-            cachedServer = server;
-            return handleRequest(event, cachedServer, context);
-        });
-    } else {
-        return handleRequest(event, cachedServer, context);
-    }
+  if (!cachedServer) {
+    bootstrapServer().then(server => {
+      cachedServer = server;
+      return handleRequest(event, cachedServer, context);
+    });
+  } else {
+    return handleRequest(event, cachedServer, context);
+  }
 };
 
 async function bootstrapServer(): Promise<Server> {
-    const app = await NestFactory.create(AppModule, express);
-    app.enableCors();
-    app.use(bodyParser.text());
-    app.useGlobalPipes(new ValidationPipe({
-        forbidNonWhitelisted: true,
-    }));
+  const app = await NestFactory.create(AppModule, express);
+  app.enableCors();
+  app.use(bodyParser.text());
+  app.useGlobalPipes(new ValidationPipe({
+    forbidNonWhitelisted: true,
+  }));
 
-    await app.init();
-    return serverless.createServer(express);
+  await app.init();
+  return serverless.createServer(express);
 }
 
 function handleRequest(event: AWSLambda.APIGatewayEvent, server, context: Context) {
-    return serverless.proxy(
-        server,
-        event,
-        context
-    );
+  return serverless.proxy(
+      server,
+      event,
+      context
+  );
 }
