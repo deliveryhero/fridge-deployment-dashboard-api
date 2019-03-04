1e6c5d7178ee2043d4a3bc2555e7447f7490441f
Mohamed Azouz <mohamedaliazouz@gmail.com>
2019-03-04 15:10:02 +0100

    T-2 delete *.d.ts

--- src/custom-validation-decorator/FridgeCustomValidationDecorators.d.ts
+++
@@ -1,2 +0,0 @@
-import { ValidationOptions } from 'class-validator';
-export declare function IsFridgeISO8601WithoutMs(validationOptions?: ValidationOptions): (object: Object, propertyName: string) => void;
