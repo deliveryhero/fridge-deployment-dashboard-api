e6de86653bd87836706be309c4920b662ddcd636
Alexey Alexandrov <alexey.aleksandrov@deliveryhero.com>
2019-03-05 15:37:27 +0100

    Bugfixes and method templates

--- src/deployment/deployments.controller.ts
+++ src/deployment/deployments.controller.ts
@@ -1,9 +1,19 @@
-import {Controller, Post} from '@nestjs/common';
+import {Controller, Get, Post} from '@nestjs/common';
 
 @Controller('/v1/deployments')
 export class DeploymentsController {
   @Post()
   addDeployment() {
-    return 'This action returns all cats';
+    return 'This action adds a deployment';
+  }
+
+  @Get('/:teamName')
+  getEnvironments() {
+    return 'This action returns all environments';
+  }
+
+  @Get('/:teamName/:environmentName')
+  getDeployments() {
+    return 'This action returns all deployments for a team and environment';
   }
 }
