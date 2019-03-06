197cf029ffc2a65e427d79af1b223f556a90db84
Mohamed Azouz <mohamedaliazouz@gmail.com>
2019-03-06 12:23:22 +0100

    T-4 remove serverless folder from git, remove accountId

--- src/application-configuration-factory.ts
+++ src/application-configuration-factory.ts
@@ -2,7 +2,6 @@ import {ApplicationConfig} from './application-configuration';
 
 const config = new ApplicationConfig(process.env.STAGE, {
     region: process.env.AWS_REGION,
-    accountId: process.env.AWS_ACCOUNT_ID,
     dynamoDB: {
       endpoint: process.env.DYNAMO_DB_ENDPOINT,
       tables: {
@@ -13,11 +12,5 @@ const config = new ApplicationConfig(process.env.STAGE, {
     },
   }
 );
-
-// if (process.env.STAGE === 'test') {
-//   AWS.config.update({
-//     credentials: new AWS.Credentials('', ''),
-//   });
-// }
 
 export { config };
